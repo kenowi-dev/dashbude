f81026102434f497d3b74fa2e5450e16726020e6
Till Wittenburg <wittenburg.till+git@gmail.com>
2025-03-08 14:46:47 +0100

    fix: unset env variable in build

--- src/lib/server/dashboard-watcher.ts
+++ src/lib/server/dashboard-watcher.ts
@@ -2,21 +2,26 @@ import * as fs from "node:fs";
 import {env} from "$env/dynamic/private";
 import {logger} from "$lib/server/logger";
 import { Dashbaord} from "$lib/types";
+import {building} from "$app/environment";
 
 let dashboard: Dashbaord = new Dashbaord();
 
 function loadItems(): void {
-    const itemsText = fs.readFileSync(env.ITEMS_JSON_PATH, 'utf8')
+    const itemsText = fs.readFileSync(env.ITEMS_JSON_PATH || "", 'utf8')
     dashboard = JSON.parse(itemsText);
     logger.info(`dashboard reloaded with ${dashboard.items.length} items`);
 }
 
+if (!building) {
+    if (!env.ITEMS_JSON_PATH) {
+        logger.error("ITEMS_JSON_PATH not set");
+    }
 
-fs.watchFile(env.ITEMS_JSON_PATH, () => {
+    fs.watchFile(env.ITEMS_JSON_PATH || "", () => {
+        loadItems();
+    })
     loadItems();
-})
-
-loadItems();
+}
 
 export {
     dashboard
